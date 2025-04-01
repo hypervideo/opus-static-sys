7582abd1d8a45c761eceede9f671f90c9110bbcf
Will Hedgecock <will.hedgecock@gmail.com>
2025-04-01 12:32:10 -0500

    Ensure build.rs writes correct lib name based on OS

--- build.rs
+++ build.rs
@@ -29,9 +29,19 @@ fn main() {
     .define("CMAKE_INTERPROCEDURAL_OPTIMIZATION", "TRUE")
     .build();
   println!("cargo:root={}", dest.display());
+#[cfg(target_os = "windows")]
+{
+  println!("cargo:include={}\\include", dest.display());
+  println!("cargo:lib_path={}\\lib", dest.display());
+  println!("cargo:lib={}\\lib\\opus.lib", dest.display());
+  println!("cargo:rustc-link-search=native={}\\lib", dest.display());
+}
+#[cfg(not(target_os = "windows"))]
+{
   println!("cargo:include={}/include", dest.display());
   println!("cargo:lib_path={}/lib", dest.display());
   println!("cargo:lib={}/lib/libopus.a", dest.display());
   println!("cargo:rustc-link-search=native={}/lib", dest.display());
+}
   println!("cargo:rustc-link-lib=static=opus");
 }
